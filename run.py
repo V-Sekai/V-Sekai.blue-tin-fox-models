7e5c25a5e9643a34299e79b6b4cc989bb57630a4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-06-19 00:44:22 -0700

    Use proper name for vrm.

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ import shutil
 import os
 
 bpy.ops.preferences.addon_install(filepath='./1_11_0.zip')
-bpy.ops.preferences.addon_enable(module="1_11_0")
+bpy.ops.preferences.addon_enable(module="VRM_Addon_for_Blender-1_11_0")
 
 directories = ["characters"]
 export_path = "result"
