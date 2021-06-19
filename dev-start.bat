980405efca3277e272f8658ffd0e331a377a183b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-06-18 22:52:47 -0700

    Try exporting.

---
+++ dev-start.bat
@@ -0,0 +1,9 @@
+scoop install kubectl
+k3d cluster delete k3s-default
+k3d cluster create --agents=3 --volume c:/out:/tmp/out@agent[0,1,2]
+kubectl config use-context k3d-k3s-default
+kubectl cluster-info
+kubectl create ns argo
+kubectl apply -n argo --wait=true -f quick-start-postgres.yml
+kubectl rollout status deployment/argo-server -n argo
+argo submit -n argo --log argo-gltf-export.yml
